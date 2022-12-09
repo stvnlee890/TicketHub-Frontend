2a48aa26cd1fa993424e186fb10b565850071896
stvnlee890 <stvnlee890@gmail.com>
2022-12-08 19:03:39 -0800

    created the login layout and able to successfully login user and provide jwt token

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserLogin } from "../../api/HttpRequest";
 
 const Login = () => {
+    const navigate = useNavigate()
+  const initialFormState = {
+    email: "",
+    password: "",
+  };
+  const [login, setLogin] = useState(initialFormState);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setLogin({ ...login, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    UserLogin(login).then((res) => {
+        console.log(res)
+      if (res.status === 200) {
+        window.localStorage.setItem("token", res.token);
+        window.localStorage.setItem("isLoggedIn", true);
+        navigate("/")
+      } else if(res.statusCode === 422){
+        alert('email or password is incorrect')
+        setLogin(initialFormState)
+      }
+    });
+  };
   return (
     <div>
-      <form>
-        <input placeholder="email"></input>
-        <input placeholder="password"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          id="email"
+          placeholder="email"
+          value={login.email}
+        ></input>
+        <input
+          onChange={handleChange}
+          id="password"
+          placeholder="password"
+          type="password"
+          value={login.password}
+        ></input>
         <button>Login</button>
       </form>
     </div>
