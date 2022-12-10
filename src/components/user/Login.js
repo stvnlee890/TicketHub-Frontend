7837b377cdf3e1270abf34601743e398a91d0066
stvnlee890 <stvnlee890@gmail.com>
2022-12-09 21:01:54 -0800

    added regex for handling password

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserLogin } from "../../api/HttpRequest";
+import { checkLoggedIn } from "../../store/LoggedInSlice";
 
 const Login = () => {
     const navigate = useNavigate()
