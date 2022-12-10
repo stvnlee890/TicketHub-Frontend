07e01c249bce2446a61b676ca3a127f87424112b
stvnlee890 <stvnlee890@gmail.com>
2022-12-09 23:11:25 -0800

    changed the file structure and name so had to update index with proper name

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import Store from "./store/Store";
+import { store } from "./store/Store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <Provider store={Store}>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
-    </Provider>
   </Router>
 );
