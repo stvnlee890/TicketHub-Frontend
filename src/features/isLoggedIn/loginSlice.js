22c2e960d1ebb4af226f648a3400edc1752e55f0
stvnlee890 <stvnlee890@gmail.com>
2022-12-09 23:24:08 -0800

    added comments to understand how Redux toolkit uses immer

--- src/features/isLoggedIn/loginSlice.js
+++ src/features/isLoggedIn/loginSlice.js
@@ -1,24 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 // think of slice as a feature of an app
 const initialState = {
   isLoggedIn: false,
 };
-const logInStatus = window.localStorage.getItem("isLoggedIn");
 const loginSlice = createSlice({
-    // create slice requires a name, state, reducers property
+  // create slice requires a name, state, reducers property
   name: "isLoggedIn",
   initialState,
   reducers: {
     // handle the reducers logic here
     // able to mutate state in reducer through **immer** which occurs under the hood
+    // we don't actually mutate the state because of immer library.
+    // immer detects changes to a "draft state" and produces a
+    // brand new immutable state based off those changes
     setLoggedIn: (state) => {
       state.isLoggedIn = true;
       //   else state.isLoggedIn = false
     },
     setLogOut: (state) => {
-        state.isLoggedIn = false;
-    }
+      state.isLoggedIn = false;
+    },
   },
 });
 
