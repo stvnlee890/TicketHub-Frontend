bef075f4ed787889d81097be81317c1555141fac
stvnlee890 <stvnlee890@gmail.com>
2022-12-11 14:59:26 -0800

    import useContext hook and AuthContext to provide update state of user login to conditionally render nav links

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,41 +1,16 @@
-import React, {useState, useEffect } from "react";
+import React, { useContext } from "react";
+import AuthContext from "../../context/AuthProvider";
 import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 const Nav = () => {
-  // our store is provided to Nav component through useSelector
-  // this is the state (in this case store) needed in order to handle
-  // logic based on the change of state.
-  // const loggedIn = useSelector((store) => store.loggedIn.isLoggedIn);
-  const [loggedIn, setLoggedIn] = useState(false);
-  const isLoggedIn = window.localStorage.getItem('isLoggedIn');
-  window.addEventListener('storage', () => {
-    if(isLoggedIn === 'true') setLoggedIn(true)
-  })
-
-  console.log(loggedIn)
-  // useEffect(() => {
-  //   const checkStorage = () => {
-  //     const isLoggedIn = window.localStorage.getItem('isLoggedIn');
-  //     console.log(isLoggedIn)
-  //     if (isLoggedIn === 'true') setLoggedIn(true);
-  //   }
-  //   window.addEventListener('storage', checkStorage)
-
-  //   return () => {
-  //     window.removeEventListener('storage', checkStorage)
-  //   }
-  // },[])
-  // console.log(loggedIn)
-
-  
-
+  const context = useContext(AuthContext)
+  console.log(context.isLoggedIn)
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
-      {!loggedIn && <NavLink to="/signup">Sign Up</NavLink>}
-      {!loggedIn && <NavLink to="/login">Login</NavLink>}
-      {loggedIn && <NavLink to="/logout">Logout</NavLink>}
+      {!context.isLoggedIn && <NavLink to="/signup">Sign Up</NavLink>}
+      {!context.isLoggedIn && <NavLink to="/login">Login</NavLink>}
+      {context.isLoggedIn && <NavLink to="/logout">Logout</NavLink>}
     </nav>
   );
 };
