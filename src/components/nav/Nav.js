b8f161d7b98e57f7111e934c75965b641e8c1156
stvnlee890 <stvnlee890@gmail.com>
2022-12-11 18:15:46 -0800

    provided the component with useSelector of the current state of based on data stored in local storage

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,16 +1,18 @@
 import React, { useContext } from "react";
-import AuthContext from "../../context/AuthProvider";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Nav = () => {
-  const context = useContext(AuthContext)
+  const isLoggedIn = useSelector((state) => state.reducer.userLogIn.isLoggedIn.payload.isLoggedIn)
+  // const isLoggedIn = useSelector((state) => console.log(state))
+  // console.log(isLoggedIn)
 
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
-      {!context.isLoggedIn && <NavLink to="/signup">Sign Up</NavLink>}
-      {!context.isLoggedIn && <NavLink to="/login">Login</NavLink>}
-      {context.isLoggedIn && <NavLink to="/logout">Logout</NavLink>}
+      {!isLoggedIn && <NavLink to="/signup">Sign Up</NavLink>}
+      {!isLoggedIn && <NavLink to="/login">Login</NavLink>}
+      {isLoggedIn && <NavLink to="/logout">Logout</NavLink>}
     </nav>
   );
 };
