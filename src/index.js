f78ff21b77c85f9c0aea7ed4ed9474f205b9c7ca
stvnlee890 <stvnlee890@gmail.com>
2022-12-11 18:18:25 -0800

    configured components for redux persist

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { store } from "./store/Store";
+import { store, persistor } from "./store/Store";
 import { Provider } from "react-redux";
-import { AuthProvider } from "./context/AuthProvider";
+// import { AuthProvider } from "./context/AuthProvider";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <React.StrictMode>
       <Provider store={store}>
-        <AuthProvider>
+        <PersistGate loading={null} persistor={persistor}>
           <App />
-        </AuthProvider>
+        </PersistGate>
+        {/* <AuthProvider> */}
+        {/* </AuthProvider> */}
       </Provider>
     </React.StrictMode>
   </Router>
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,44 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
 import loggedInReducer from "../features/isLoggedIn/loginSlice";
 
+const reducers = combineReducers({
+  userLogIn: loggedInReducer,
+});
+
+const persistConfig = {
+  key: "userInfo",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 // we import the loginSlice.reducer into the store
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     // name is loggedInReducer because we exported loginSlice.reducer as a default
-    loggedIn:loggedInReducer
+    reducer: persistedReducer,
   },
-});  
+  middleware: (getDefaultMiddleWare) =>
+    getDefaultMiddleWare({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export {store, persistor}
