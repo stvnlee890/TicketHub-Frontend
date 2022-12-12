775550abbef2808b52883b02ab927af5ec00fbaf
stvnlee890 <stvnlee890@gmail.com>
2022-12-11 18:18:02 -0800

    configured redux-persit to persist state stored in local storage

--- src/features/isLoggedIn/loginSlice.js
+++ src/features/isLoggedIn/loginSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 // think of slice as a feature of an app
 
 const initialState = {
-  isLoggedIn: false,
+  isLoggedIn: {},
 };
 
 const loginSlice = createSlice({
@@ -15,14 +15,14 @@ const loginSlice = createSlice({
     // we don't actually mutate the state because of immer library.
     // immer detects changes to a "draft state" and produces a
     // brand new immutable state based off those changes
-    setLoggedIn: (state) => {
-      console.log(state.isLoggedIn)
-      state.isLoggedIn = window.localStorage.getItem('user');
+    setLoggedIn: (state, action) => {
+      state.isLoggedIn = action
       console.log(state.isLoggedIn)
       //   else state.isLoggedIn = false
     },
-    setLogOut: (state) => {
-      state.isLoggedIn = false;
+    setLogOut: (state, action) => {
+      state.isLoggedIn = action;
+      console.log(state.isLoggedIn)
     },
   },
 });
