16f7ef99fac7df0838d22870f9c799ba3adbeb0f
stvnlee890 <stvnlee890@gmail.com>
2022-12-12 23:51:04 -0800

    Display the different ticket tier of that particular artist

--- src/components/concerts/concertDetail/ConcertDetail.js
+++ src/components/concerts/concertDetail/ConcertDetail.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { GetTicketDetails } from "../../../api/HttpRequest";
+import Tickets from "./Tickets";
 
 const ConcertDetail = () => {
-    return (
-        <div>Concert Detail</div>
-    )
-}
+  const { concertId } = useParams("concertId");
+  const [tickets, setTickets] = useState()
+
+  useEffect(() => {
+    GetTicketDetails(concertId).then(res => setTickets(res))
+  },[])
+  return (
+    <div>
+        <h1>Concert Details</h1>
+        {!tickets ? <p>loading</p> :
+        tickets.map((ticket) => (
+            <Tickets key={ticket._id} ticket={ticket}/>
+        ))
+        }
+    </div>
+  )
+};
 
 export default ConcertDetail;
