af420fe3632c36c9769157fe38e0f53bb1a2a8fb
stvnlee890 <stvnlee890@gmail.com>
2022-12-12 22:43:40 -0800

    created a http get request to fetch all concerts on home page

--- src/api/HttpRequest.js
+++ src/api/HttpRequest.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:8800/api/tickethub";
 
+// ===================
+// USER LOGIN
+// ===================
 // USER SIGN UP REQUEST
 export const UserSignup = async (body) => {
   try {
@@ -19,5 +22,18 @@ export const UserLogin = async (body) => {
     return res.data;
   } catch (error) {
     console.log(error);
+  }
+};
+
+// ===================
+// CONCERTS
+// ===================
+
+export const GetConcerts = async() => {
+  try {
+    const res = await axios.get(`${baseUrl}/concerts`);
+    return res.data;
+  } catch (error) {
+    console.log(error)
   }
 };
