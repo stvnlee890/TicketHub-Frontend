f29b6f153f24068cd4ad1e0136403bdec304a100
stvnlee890 <stvnlee890@gmail.com>
2022-12-12 19:43:22 -0800

    passed action as empty object in dispatch function

--- src/components/user/Logout.js
+++ src/components/user/Logout.js
@@ -1,15 +1,11 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setLogOut } from "../../features/isLoggedIn/loginSlice";
-
 
 const Logout = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(setLogOut({
-      token: null,
-      isLoggedIn: false,
-    }))
+    dispatch(setLogOut({}))
   }, []);
 
   return <div>Logout successful</div>;
