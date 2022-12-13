383b3709a0a57f3e964aad4f242b3aefa627f46c
stvnlee890 <stvnlee890@gmail.com>
2022-12-12 22:44:42 -0800

    fetched all concerts with useEffect, and mapped each concert to display on ConcertDetail component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { GetConcerts } from "../../api/HttpRequest";
+import Concerts from "../concerts/allConcerts/Concerts";
 
 const Home = () => {
-    return(
-        <section>Home</section>
-    )
-}
+  const [concerts, setConcerts] = useState();
+
+  useEffect(() => {
+    GetConcerts().then((res) => setConcerts(res));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to TicketHub</h1>
+      <h2>Current Shows</h2>
+      {!concerts ? (
+        <p>loading</p>
+      ) : (
+        concerts.map((concert) => (
+          <Concerts key={concert._id} concert={concert} />
+        ))
+      )}
+    </div>
+  );
+};
 
 export default Home;
