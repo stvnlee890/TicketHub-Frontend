98eced955d085bb196b2337561a5e001b21f5616
stvnlee890 <stvnlee890@gmail.com>
2022-12-12 19:44:32 -0800

    configured store so we can persist state in local storage for user login

--- src/store/Store.js
+++ src/store/Store.js
@@ -7,25 +7,42 @@ import {
   FLUSH,
   REHYDRATE,
   PAUSE,
-  PERSIST,
   PURGE,
   REGISTER,
 } from "redux-persist";
 
+// this is the login slice
 import loggedInReducer from "../features/isLoggedIn/loginSlice";
 
+
+// Typically, configureStore handles our need of having to combineReducers but in this case, 
+// we might have to call combineReducers to pass into persitReducer method
+// combineReducers turns an object whose values are different reducing functions
+// into a single reducing function you can pass to createState
+// we have multiple reducers in loginSlice: setLoggedIn, setLogOut
 const reducers = combineReducers({
-  userLogIn: loggedInReducer,
+  userHandle: loggedInReducer,
 });
 
+// initialize persistConfig to pass as first argument to persistReducer
+// required configs:
+  // key, storage
+  // additional configs, whitelist, blacklist, version, stateReconciler
 const persistConfig = {
   key: "userInfo",
   storage,
 };
 
+// persistReducer takes in the reducer function. 
+// typically this would be a top level reducer returned by combineReducers
 const persistedReducer = persistReducer(persistConfig, reducers);
 
-// we import the loginSlice.reducer into the store
+// configureStore
+// by default, configureStore adds some middleware (unless customize) automatically.
+// when supplying a middleware, you're responsible for all middleware added to store.
+// configure store will not add any extre middleware beyond what you listed.  
+// getDefaultMiddleware is useful if you want to add some custom middleware, 
+// but also still want to have the default middleware added as well:
 const store = configureStore({
   reducer: {
     // name is loggedInReducer because we exported loginSlice.reducer as a default
